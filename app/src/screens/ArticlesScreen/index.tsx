98efb825292e99c7e48ef7f38314a7597cc3fb05
Alex Mitchell <amitchell@unicef.org>
2024-11-14 14:42:49 +0700

    Audio player for article voice over

--- app/src/screens/ArticlesScreen/index.tsx
+++ app/src/screens/ArticlesScreen/index.tsx
@@ -11,6 +11,7 @@ import { useSelector } from '../../redux/useSelector'
 import { articlesSelector, subCategoryByIDSelector } from '../../redux/selectors'
 import { globalStyles } from '../../config/theme'
 import { useColor } from '../../hooks/useColor'
+import { AudioPlayer } from '../../components/AudioPlayer'
 
 const ArticlesScreen: ScreenComponent<'Articles'> = ({ navigation, route }) => {
   const { backgroundColor, palette } = useColor()
@@ -45,7 +46,10 @@ const ArticlesScreen: ScreenComponent<'Articles'> = ({ navigation, route }) => {
     <Screen>
       <ScrollView style={styles.scrollView} contentContainerStyle={styles.contentContainer}>
         <SearchBar query={query} setQuery={setQuery} style={globalStyles.shadow} />
-        {articles?.map((article) => {
+        {articles?.map((article, i) => {
+          const audioAssetUri = audios[i] ?? undefined
+          // const audioAssetUri =  `${AWS_S3_BASE_URL}/${article.voiceOverKey}` TODO:
+
           return (
             <View style={[styles.card, { backgroundColor }, globalStyles.shadow]} key={article.id}>
               {article.title && (
@@ -62,6 +66,8 @@ const ArticlesScreen: ScreenComponent<'Articles'> = ({ navigation, route }) => {
               >
                 {subcategory.name}
               </Text>
+
+              <AudioPlayer audioAssetUri={audioAssetUri} />
               <ArticleContent articleId={article.id} text={article.content} />
             </View>
           )
@@ -98,3 +104,19 @@ const styles = StyleSheet.create({
     marginBottom: 24,
   },
 })
+
+// TODO: remove
+
+const audio0 =
+  'https://firebasestorage.googleapis.com/v0/b/periodtracker-cb22c.appspot.com/o/10cbc9dd-a421-40c4-a4d4-9478845fcd91-file_example_ERROR_mp3_700kb.mp3?alt=media'
+
+const audio1 =
+  'https://firebasestorage.googleapis.com/v0/b/periodtracker-cb22c.appspot.com/o/10cbc9dd-a421-40c4-a4d4-9478845fcd91-file_example_mp3_700kb.mp3?alt=media'
+
+const audio2 =
+  'https://firebasestorage.googleapis.com/v0/b/periodtracker-cb22c.appspot.com/o/ca7e3ce1-6e04-4bc1-b986-63b9eb6bf08f-file_example_mp3_700kb.mp3?alt=media'
+
+const audio3 =
+  'https://firebasestorage.googleapis.com/v0/b/periodtracker-cb22c.appspot.com/o/d4b08b3f-b84a-44a9-a54c-2dc2c3a2b611-file_example_mp3_700kb.mp3?alt=media'
+
+const audios = [audio0, audio1, audio2, audio3]
